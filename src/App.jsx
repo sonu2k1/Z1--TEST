bc00f55d42d9a6d816f550112fcac901b45f7784
Sonu Singh <160756068+sonu2k1@users.noreply.github.com>
2025-03-04 18:28:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-import MovieSearch from './MovieSearch'
+
 import WeatherDashboard from './WeatherDashboard'
 import Pokiemon from './Pokiemon'
 import CatGallery from './CatGallery'
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <MovieSearch/>
+  
     <WeatherDashboard/>
     <Pokiemon/>
     <CatGallery/>
